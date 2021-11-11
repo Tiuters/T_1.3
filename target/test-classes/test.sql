90bbb1ff8b9a1dce4a7074cf5b3c8f3845d6a227
Tiuters <ands458@gmail.com>
2021-11-11 23:56:11 +0300

    Работает?

---
+++ target/test-classes/test.sql
@@ -0,0 +1,6 @@
+#insert into users (name, age, email) values('Bob', 30, 'st@bhjkfd.com');
+#select name, age from users;
+#update users set name='Steve', age=55 where id=1;
+SELECT * FROM users;
+#delete from users;
+#delete from users where id=4;
