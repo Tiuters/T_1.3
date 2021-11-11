6478bf9ad4d2847ce1edfbf5449b8ff2885adb23
Tiuters <ands458@gmail.com>
2021-11-11 23:55:11 +0300

    Работает?

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,6 +3,8 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 import java.util.List;
@@ -11,19 +13,18 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-//        Util util = new Util();
+        UserService userService = new UserServiceImpl();
 
-//        Util.getConnection();
-        userDaoJDBC.createUsersTable();
-        userDaoJDBC.saveUser("fff", "ggg", (byte) 45);
 
-        List<User> toPr = userDaoJDBC.getAllUsers();
+        userService.createUsersTable();
+        userService.saveUser("fff", "ggg", (byte) 45);
+
+        List<User> toPr = userService.getAllUsers();
         System.out.println(toPr);
 
-        userDaoJDBC.dropUsersTable();
+        userService.dropUsersTable();
 
-        List<User> toPr2 = userDaoJDBC.getAllUsers();
+        List<User> toPr2 = userService.getAllUsers();
         System.out.println(toPr2);
 
     }
